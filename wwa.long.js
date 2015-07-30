fe1a28c3f044fe56f67bba36d017023ba80e55cc
Matsuyuki(rmn.) <matsuyuki.eca@gmail.com>
2015-07-30 09:53:49 +0900

    RELEASE W3.14++

--- wwa.long.js
+++ wwa.long.js
@@ -666,7 +666,7 @@ var wwa_data;
     var WWAConsts = (function () {
         function WWAConsts() {
         }
-        WWAConsts.VERSION_WWAJS = "W3.14+";
+        WWAConsts.VERSION_WWAJS = "W3.14++";
         WWAConsts.WWA_HOME = "http://wwajp.com";
         WWAConsts.ITEMBOX_SIZE = 12;
         WWAConsts.MAP_ATR_MAX = 60;
@@ -6339,4 +6339,5 @@ var wwa_main;
         window.addEventListener("load", start);
     }
 })(wwa_main || (wwa_main = {}));
-//# sourceMappingURL=wwa.long.js.tmp.map\n
+//# sourceMappingURL=wwa.long.js.tmp.map
+
