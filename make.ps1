4e3579028541829576d587279607b850337d44b4
Aokashi <mail@aokashi.net>
2017-08-11 23:37:15 +0900

    プリルクを送る際にまずそうなものを削除

--- make.ps1
+++
@@ -1,5 +0,0 @@
-tsc --sourceMap .\src\wwa_main.ts -t ES5 --outDir .. --out wwa.long.js.tmp
-type wwa_license_comment.js wwa.long.js.tmp > wwa.long.js
-echo "\n" >> wwa.long.js
-
-java -jar .\closure\compiler.jar < wwa.link.js > wwa.js
