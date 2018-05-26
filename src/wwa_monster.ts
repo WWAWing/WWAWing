e4fafdf6906ae919f306229ede3afb5c8af1d2e3
Matsuyuki/rmn. (@rmn_e) <matsuyuki.eca@gmail.com>
2018-05-26 23:15:49 +0900

    chore(wwa_main): モジュールをimport/export

--- src/wwa_monster.ts
+++ src/wwa_monster.ts
@@ -1,35 +1,31 @@
-﻿/// <reference path="./wwa_main.ts" />
+﻿import { Coord, Status } from "./wwa_data";
+export class Monster {
 
-module wwa_monster {
-    export class Monster {
+    constructor(
+        private _partsID: number,
+        private _position: Coord,
+        private _imgCoord: Coord,
+        private _status: Status,
+        private _message: string,
+        private _item: number,
+        private _battleEndCallback: () => void
+    ) {
 
-        constructor(
-            private _partsID: number,
-            private _position: wwa_data.Coord,
-            private _imgCoord: wwa_data.Coord,
-            private _status: wwa_data.Status,
-            private _message: string,
-            private _item: number,
-            private _battleEndCallback: () => void
-            ) {
+    }
 
-        }
+    get partsID(): number { return this._partsID; }
+    get position(): Coord { return this._position; }
+    get imgCoord(): Coord { return this._imgCoord; }
+    get status(): Status { return this._status; }
+    get message(): string { return this._message; }
+    get item(): number { return this._item; }
 
-        get partsID(): number { return this._partsID; }
-        get position(): wwa_data.Coord { return this._position; }
-        get imgCoord(): wwa_data.Coord { return this._imgCoord; }
-        get status(): wwa_data.Status { return this._status; }
-        get message():string { return this._message; }
-        get item(): number { return this._item; }
+    public damage(amount: number): void {
+        this._status.energy = Math.max(0, this._status.energy - amount);
+    }
 
-        public damage( amount: number): void {
-            this._status.energy = Math.max(0, this._status.energy - amount);
-        }
-
-        public battleEndProcess(): void {
-            this._battleEndCallback();
-        }
-
+    public battleEndProcess(): void {
+        this._battleEndCallback();
     }
 
 }
--- src/wwa_motion.ts
+++ src/wwa_motion.ts
@@ -1,119 +1,116 @@
-﻿/// <reference path="./wwa_data.ts" />
-
-module wwa_motion {
-
-    import Dir = wwa_data.Direction;
-    export class ObjectMovingData {
-        private _currentPos: wwa_data.Position;
+﻿import { WWA } from "./wwa_main";
+import { Direction as Dir, Position, speedList, PartsType, Coord, Direction } from "./wwa_data";
+import { Player } from "./wwa_parts_player";
 
 
-        public constructor(
-            private _player: wwa_parts_player.Player,
-            private _objectID: number,
-            private _srcPos: wwa_data.Position,
-            private _destPos: wwa_data.Position,
-            private _dir: Dir
-            ) {
-            this._currentPos = this._srcPos.clone();
-        }
+export class ObjectMovingData {
+    private _currentPos: Position;
 
-        public update(): wwa_data.Position {
-            var speedIndex = this._player.getSpeedIndex();
-            this._currentPos = this._currentPos.getNextFramePosition(
-                this._dir,
-                wwa_data.speedList[ speedIndex ],
-                wwa_data.speedList[ speedIndex ]);
 
-            return this._currentPos;
-        }
+    public constructor(
+        private _player: Player,
+        private _objectID: number,
+        private _srcPos: Position,
+        private _destPos: Position,
+        private _dir: Dir
+    ) {
+        this._currentPos = this._srcPos.clone();
+    }
 
-        public get isAchievedDestination(): boolean {
-            return this._currentPos.equals(this._destPos);
-        }
+    public update(): Position {
+        var speedIndex = this._player.getSpeedIndex();
+        this._currentPos = this._currentPos.getNextFramePosition(
+            this._dir,
+            speedList[speedIndex],
+            speedList[speedIndex]);
 
-        public get currentPosition(): wwa_data.Position {
-            return this._currentPos;
-        }
+        return this._currentPos;
+    }
 
-        public get beforePosition(): wwa_data.Position {
-            return this._srcPos;
-        }
+    public get isAchievedDestination(): boolean {
+        return this._currentPos.equals(this._destPos);
+    }
 
-        public get destination(): wwa_data.Position {
-            return this._destPos;
-        }
+    public get currentPosition(): Position {
+        return this._currentPos;
+    }
 
-        public get objID(): number {
-            return this._objectID;
-        }
+    public get beforePosition(): Position {
+        return this._srcPos;
+    }
+
+    public get destination(): Position {
+        return this._destPos;
+    }
+
+    public get objID(): number {
+        return this._objectID;
+    }
+}
+
+
+export class ObjectMovingDataManager {
+
+    private _queue: ObjectMovingData[];
+
+    // TODO: シングルトンにする
+    public constructor(private _wwa: WWA, private _player: Player) {
+        this.clear();
+    }
+
+    public add(objectID: number, srcPos: Position, destPos: Position, dir: Direction): void {
+        this._queue.push(new ObjectMovingData(this._player, objectID, srcPos, destPos, dir));
     }
 
 
-    export class ObjectMovingDataManager {
+    //  動き終わったオブジェクトからなる配列を返します。
+    public update(): ObjectMovingData[] {
+        var endObjects: ObjectMovingData[] = [];
+        var continueObjects: ObjectMovingData[] = [];
+        for (var i = 0; i < this._queue.length; i++) {
+            this._queue[i].update();
+            if (this._queue[i].isAchievedDestination) {
+                endObjects.push(this._queue[i]);
+                this._wwa.setPartsOnPosition(
+                    PartsType.OBJECT,
+                    0,
+                    this._queue[i].beforePosition.getPartsCoord()
+                );
+                this._wwa.setPartsOnPosition(
+                    PartsType.OBJECT,
+                    this._queue[i].objID,
+                    this._queue[i].destination.getPartsCoord()
+                );
 
-        private _queue: ObjectMovingData[];
-        
-        // TODO: シングルトンにする
-        public constructor( private _wwa: wwa_main.WWA, private _player: wwa_parts_player.Player) {
-            this.clear();
-        }
-
-        public add(objectID: number, srcPos: wwa_data.Position, destPos: wwa_data.Position, dir: wwa_data.Direction): void {
-            this._queue.push(new ObjectMovingData(this._player, objectID, srcPos, destPos, dir));
-        }
-        
-
-        //  動き終わったオブジェクトからなる配列を返します。
-        public update(): ObjectMovingData[] {
-            var endObjects: ObjectMovingData[] = [];
-            var continueObjects: ObjectMovingData[] = [];
-            for (var i = 0; i < this._queue.length; i++) {
-                this._queue[i].update();
-                if ( this._queue[i].isAchievedDestination) {
-                    endObjects.push(this._queue[i]);
-                    this._wwa.setPartsOnPosition(
-                        wwa_data.PartsType.OBJECT,
-                        0,
-                        this._queue[i].beforePosition.getPartsCoord()
-                        );
-                    this._wwa.setPartsOnPosition(
-                        wwa_data.PartsType.OBJECT,
-                        this._queue[i].objID,
-                        this._queue[i].destination.getPartsCoord()
-                        );
-
-                } else {
-                    continueObjects.push(this._queue[i]);
-                }
+            } else {
+                continueObjects.push(this._queue[i]);
             }
-            this._queue = continueObjects;
-            return endObjects;
+        }
+        this._queue = continueObjects;
+        return endObjects;
+    }
+
+    public clear() {
+        this._queue = [];
+    }
+
+    // crop座標と描画先座標の組の配列を返すメソッドを実装せよ
+    public get objectMovingData(): ObjectMovingData[] {
+        return this.objectMovingData;
+    }
+
+
+    // 本当はnullを返したくはないんだけど、例外を投げると重くなるので
+    public getOffsetByBeforePartsCoord(coord: Coord): Coord {
+        var result = this._queue.filter((x) => {
+            return x.beforePosition.getPartsCoord().equals(coord);
+        });
+
+        if (result.length === 0) {
+            return null;
         }
 
-        public clear() {
-            this._queue = [];
-        }
-
-        // crop座標と描画先座標の組の配列を返すメソッドを実装せよ
-        public get objectMovingData(): ObjectMovingData[] {
-            return this.objectMovingData;
-        }
-        
-
-        // 本当はnullを返したくはないんだけど、例外を投げると重くなるので
-        public getOffsetByBeforePartsCoord(coord: wwa_data.Coord): wwa_data.Coord {
-            var result = this._queue.filter((x) => {
-                return x.beforePosition.getPartsCoord().equals(coord);
-            });
-
-            if (result.length === 0) {
-                return null;
-            }
-
-            return result[0].currentPosition.getOffsetCoord();
-
-        }
-
+        return result[0].currentPosition.getOffsetCoord();
 
     }
 
