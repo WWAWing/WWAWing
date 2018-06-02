3e56b110729737e2030dd8f4ac13af98d364fe97
Matsuyuki/rmn. (@rmn_e) <matsuyuki.eca@gmail.com>
2018-06-03 02:52:04 +0900

    feat(index): htmlファイル書き出し

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
+import * as fs from "fs";
 import * as pug from "pug";
 import * as path from "path";
 import { WWAPageConfig, fillDefaultConfig } from "./config";
 
 const customPageConfig: WWAPageConfig = {
-    wwa: {
-        mapdata: "island02.dat"
-    }
+  wwa: {
+    mapdata: "island02.dat"
+  }
 };
 
+const pugFileNameWithoutExtention = "wwa";
 const pageConfig = fillDefaultConfig(customPageConfig);
-const pugFile = path.join(__dirname, "..", "template", "wwa.pug");
-const compileTemplate = pug.compileFile(pugFile, { pretty: true });
-console.log(compileTemplate(pageConfig));
+const pugFilePath = path.join(
+  __dirname,
+  "..",
+  "template",
+  `${pugFileNameWithoutExtention}.pug`
+);
+const compileTemplate = pug.compileFile(pugFilePath, { pretty: true });
+const html = compileTemplate(pageConfig);
+fs.writeFileSync(
+  path.join(__dirname, "..", "output", `${pugFileNameWithoutExtention}.html`),
+  html
+);
