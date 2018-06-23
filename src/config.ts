4335f0a27327ed8dd0bf429fd3390dd0a8d5efe4
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-06-24 03:29:16 +0900

    fix(config): スキームが間違っていたので修正

--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,35 @@
 import * as config from "config";
 import * as jsonschema from "jsonschema";
 
+// TODO: TS型定義自動生成
+// @see https://github.com/bcherny/json-schema-to-typescript
 export interface WWAConfig {
     urlgateEnable: boolean;
-    resources: {
+    resources?: {
         mapdata: string;
-        loader: string;
-        audio: {
-            dir: string;
-            js: string;
+        loader?: string;
+        audio?: {
+            dir?: string;
+            js?: string;
         };
-        wwaJs: string;
-        wwaCss: string;
-        titleImg: string;
+        wwaJs?: string;
+        wwaCss?: string;
+        titleImg?: string;
         cryptoJsInDevMode?: string;
     };
 }
 
 export interface Copyright {
-    startYear: number;
-    presentYear: number | "latest";
+    range?: {
+        firstYear: number;
+        lastYear?: number | "present";
+    },
     product: {
         name: string;
         href: string;
     };
     credit: string;
-    genre: string;
+    genre?: string;
 }
 
 export interface WWAPageConfig {
@@ -38,44 +42,68 @@ export interface WWAPageConfig {
 const schema = {
     properties: {
         wwa: {
-            urlgateEnable: { type: "boolean" },
-            resources: {
-                mapdata: { type: "string" },
-                audio: {
-                    dir: { type: "string" },
-                    js: { type: "string" },
-                },
-                wwaJs: { type: "string" },
-                wwaCss: { type: "string" },
-                titleImg: { type: "string" },
-                cryptoJsInDevMode: { type: "boolean" }
+            properties: {
+                urlgateEnable: { type: "boolean" },
+                resources: {
+                    properties: {
+                        mapdata: { type: "string" },
+                        audio: {
+                            properties: {
+                                dir: { type: "string" },
+                                js: { type: "string" }
+                            }
+                        },
+                        wwaJs: { type: "string" },
+                        wwaCss: { type: "string" },
+                        titleImg: { type: "string" },
+                        cryptoJsInDevMode: { type: "string" }
+                    },
+                    required: ["mapdata"]
+                }
             }
         },
-        copyright: {
+        copyrights: {
             type: "array",
             items: {
-                startYear: { type: "number" },
-                presentYear: { type: "number" },
-                credit: { type: "string" },
-                product: {
-                    name: { type: "string" },
-                    href: { type: "string" }
+                properties: {
+                    range: {
+                        properties: {
+                            firstYear: {type: "number"},
+                            lastYear: {
+                                type: ["number", "string"],
+                                pattern: "^present$"
+                            }
+                        },
+                        required: ["firstYear"]
+                    },
+                    credit: { type: "string" },
+                    product: {
+                        properties: {
+                            name: { type: "string" },
+                            href: { type: "string" }
+                        },
+                        required: ["name", "href"]
+                    },
+                    required: ["startYear", "credit", "product"]
                 }
             }
         }
     },
+    required: ["wwa"]
 };
 
 export function readConfig(): WWAPageConfig {
+    let validationErrors: string[] = [];
+
+    function validate(config: any): config is WWAPageConfig {
+        const validateResult = jsonschema.validate(config, schema);
+        validationErrors = validateResult.errors.map(e => e.message);
+        return validateResult.valid;
+    }
+
     const wwaPageConfig = config.get("page");
     if (validate(wwaPageConfig)) {
         return wwaPageConfig;
     }
-    throw new Error("ERROR");
-}
-
-export function validate(config: any): config is WWAPageConfig {
-    const validateResult = jsonschema.validate(config, schema);
-    console.log(validateResult);
-    return validateResult.valid;
+    throw new Error(validationErrors[0]);
 }
