82ed583de90ed1bde62215dc5b7946a80d920675
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-06-30 23:11:20 +0900

    chore(debug): cliをデバッグディレクトリに切り離し

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as fs from "fs";
 import * as pug from "pug";
 import * as path from "path";
 import { readConfig } from "./config";
@@ -7,13 +6,5 @@ export function createPage(configFilePath: string): string {
   const pageConfig = readConfig(configFilePath);
   const pugFilePath = path.join(__dirname, path.normalize(pageConfig.page.template));
   const compileTemplate = pug.compileFile(pugFilePath, { pretty: true });
-  return compileTemplate(pageConfig);
+  return compileTemplate(pageConfig) + "\n";
 }
-
-// 暫定CLI. CLIを別パッケージに分割したら消す
-const fileName = process.argv.length < 3 ? "../config/default.yml" : process.argv[2];
-const html = createPage(fileName) + "\n";
-fs.writeFileSync(
-  path.join(__dirname, "..", "output", "wwa.html"),
-  html
-);
