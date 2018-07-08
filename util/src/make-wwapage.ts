5ab3a41ef79fade6e6a50f35c5f83987f7627f0f
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-07-09 00:38:34 +0900

    feat(make-wwapage): 開発用マップデータ生成

--- util/src/make-wwapage.ts
+++ util/src/make-wwapage.ts
@@ -1,59 +1,61 @@
-import { generateWWAPageFromConfig } from "@wwawing/html-generator";
+import { generateWWAPageFromConfig, WWAPageConfig, getDefaultCopyrights } from "@wwawing/html-generator";
 import * as fs from "fs";
 import * as path from "path";
 
 // マップデータのファイル名から .dat を取り除いたもの
-const mapNames = ["caves01", "caves02", "island02", "wwamap", "test"];
+const mapNamesDev = ["caves01", "caves02", "island02", "wwamap", "test", "g002-302", "g002-310"];
+const mapNamesDist = ["caves01", "caves02", "island02", "wwamap"];
 
-
-Promise.all(createHTMLFilePromises().concat(createHTMLFilePromises(true)))
-    .then(() => console.log("done."))
+const allPromises: Promise<void>[] = [];
+if (process.argv.length < 3 || process.argv[2] === "dist" || process.argv[2] === "all") {
+    allPromises.concat(createDistPagePromises());
+}
+if (process.argv[2] === "dev" || process.argv[2] === "all") {
+    allPromises.concat(createDevPagePromises());
+}
+Promise.all(allPromises)
     .catch(error => {
         console.error("error", error);
         process.exit(1);
     });
 
-function createHTMLFilePromises(isDevMode = false): Promise<void>[] {
+function createDistPagePromises(): Promise<void>[] {
+    return createHTMLFilePromises(mapNamesDist);
+}
+
+function createDevPagePromises(): Promise<void>[] {
+    return createHTMLFilePromises(mapNamesDev, true);
+}
+
+function createHTMLFilePromises(mapNames: string[], isDevMode = false): Promise<void>[] {
     return mapNames
         .map(mapName => ({
             mapName,
-            html: generateWWAPageFromConfig({
-                page: {
-                    additionalCssFiles: ["style.css"],
-                    isDevMode,
-                    wwa: {
-                        resources: { mapdata: mapName }
-                    },
-                    copyrights: [
-                        {
-                            product: {
-                                name: "World Wide Adventure",
-                                href: "http://wwajp.com"
-                            },
-                            credit: "NAO",
-                            range: {
-                                firstYear: 1996,
-                                lastYear: 2016
-                            },
-                            genre: "Internet RPG"
-                        },
-                        {
-                            product: {
-                                name: "WWA Wing",
-                                href: "https://wwawing.com"
-                            },
-                            credit: "WWA Wing Team",
-                            range: {
-                                firstYear: 2013,
-                                lastYear: "present"
-                            }
-                        }
-                    ]
-                }
-            })
+            html: generateWWAPageFromConfig(createConfig(mapName, isDevMode))
         }))
         .map(params => createWriteFilePromise(
-            path.join(__dirname, "out", `${params.mapName}${isDevMode ? "-dev" : ""}.html`), params.html));
+            isDevMode ?
+                path.join(__dirname, "..", "..", "debug", `${params.mapName}.html`) :
+                path.join(__dirname, "..", "..", "dist_html", `${params.mapName}.html`),
+            params.html
+        ));
+}
+
+function createConfig(mapName: string, isDevMode: boolean): WWAPageConfig {
+    return {
+        page: {
+            additionalCssFiles: ["style.css"],
+            isDevMode,
+            wwa: {
+                resources: {
+                    mapdata: mapName,
+                    titleImg: "cover.gif"
+                },
+                urlgateEnable: true
+            },
+            copyrights: getDefaultCopyrights()
+        }
+    };
 }
 
 function createWriteFilePromise(filePath: string, content: string): Promise<void> {
