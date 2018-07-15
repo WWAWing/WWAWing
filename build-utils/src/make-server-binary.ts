006d1b5795a987b33f496c519675d17d41f3d697
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-07-16 01:35:53 +0900

    feature(build-utils): nexe(実験中)

---
+++ build-utils/src/make-server-binary.ts
@@ -0,0 +1,9 @@
+import { compile } from "nexe";
+
+compile({
+    input: "../debug-server", // 暫定
+    targets: [
+        "windows-x64-9.5.0"
+    ],
+    output: "wwa-server"
+}).then(() => console.log("compile server to binary done."));
