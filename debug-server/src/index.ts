cc0ceea329f21f3fe563045e72c0e6c124708719
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-07-16 00:52:40 +0900

    feat(debug-server): サーバのdocrootを指定できるようにした

--- debug-server/src/index.ts
+++ debug-server/src/index.ts
@@ -1,6 +1,8 @@
 import * as express from "express";
 
+const baseDir = process.argv.length < 3 ? "." : process.argv[2];
+
 const app = express();
 const SERVER_PORT = 3000;
-app.use("/", express.static("debug"));
+app.use("/", express.static(baseDir));
 app.listen(SERVER_PORT, () => console.log("wwa debug listening on port 3000!"));
