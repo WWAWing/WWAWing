186c60926588ef80e6b55831303466aa8e51ed5e
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-07-16 02:15:47 +0900

    rip

--- build-utils/src/make-server-binary.ts
+++
@@ -1,9 +0,0 @@
-import { compile } from "nexe";
-
-compile({
-    input: "../debug-server/package.json", // 暫定
-    targets: [
-        "windows-x64-9.5.0"
-    ],
-    output: "wwa-server"
-}).then(() => console.log("compile server to binary done."));
