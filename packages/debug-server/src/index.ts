5b33ebdd66b1c206c27908e7d9e3f4aca559a9f4
Matsuyuki/rmn. (@rmn_e) <matsuyuki.eca@gmail.com>
2018-07-22 00:26:21 +0900

    fix(*): 細部の修正

--- packages/debug-server/src/index.ts
+++ packages/debug-server/src/index.ts
@@ -5,4 +5,9 @@ const baseDir = process.argv.length < 3 ? "." : process.argv[2];
 const app = express();
 const SERVER_PORT = 3000;
 app.use("/", express.static(baseDir));
-app.listen(SERVER_PORT, () => console.log("wwa debug listening on port 3000!"));
+app.listen(SERVER_PORT, () =>  {
+    console.log("Welcome to WWA Server!");
+    console.log(`http://localhost:${SERVER_PORT} でローカルでマップの仕上がりを確認できます。`);
+    console.log(`例: http://localhost:${SERVER_PORT}/wwamap.html`);
+    console.log("Ctrl+C もしくはこの画面を閉じると終了します。");
+});
