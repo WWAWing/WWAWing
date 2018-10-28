590a66aab31aed17372b0b875efc537c633fcc9a
Matsuyuki/rmn <matsuyuki.eca@gmail.com>
2018-10-28 15:44:23 +0900

    他もindentlessに

--- packages/engine/src/wwa_monster.ts
+++ packages/engine/src/wwa_monster.ts
@@ -1,31 +1,32 @@
-﻿import { Coord, Status } from "./wwa_data";
+import { Coord, Status } from "./wwa_data";
 export class Monster {
 
-    constructor(
-        private _partsID: number,
-        private _position: Coord,
-        private _imgCoord: Coord,
-        private _status: Status,
-        private _message: string,
-        private _item: number,
-        private _battleEndCallback: () => void
-    ) {
-
-    }
-
-    get partsID(): number { return this._partsID; }
-    get position(): Coord { return this._position; }
-    get imgCoord(): Coord { return this._imgCoord; }
-    get status(): Status { return this._status; }
-    get message(): string { return this._message; }
-    get item(): number { return this._item; }
-
-    public damage(amount: number): void {
-        this._status.energy = Math.max(0, this._status.energy - amount);
-    }
-
-    public battleEndProcess(): void {
-        this._battleEndCallback();
-    }
+constructor(
+private _partsID: number,
+private _position: Coord,
+private _imgCoord: Coord,
+private _status: Status,
+private _message: string,
+private _item: number,
+private _battleEndCallback: () => void
+) {
 
 }
+
+get partsID(): number { return this._partsID; }
+get position(): Coord { return this._position; }
+get imgCoord(): Coord { return this._imgCoord; }
+get status(): Status { return this._status; }
+get message(): string { return this._message; }
+get item(): number { return this._item; }
+
+public damage(amount: number): void {
+this._status.energy = Math.max(0, this._status.energy - amount);
+}
+
+public battleEndProcess(): void {
+this._battleEndCallback();
+}
+
+}
+
--- packages/engine/src/wwa_psave.ts
+++ packages/engine/src/wwa_psave.ts
@@ -6,31 +6,32 @@ var MASK_BIG = 0xFF00;
 var MASK_LITTLE = 0x00FF;
 
 export function getPressData(): Uint8Array {
-    return pressData;
+return pressData;
 }
 
 function pressToBuffer(
-    pos: number,
-    value: number,
-    useMax: boolean = false) {
-    if (useMax) {
-        value = Math.max(VALUE_MAX, value);
-    }
-    pressData[pos] = value;
+pos: number,
+value: number,
+useMax: boolean = false) {
+if (useMax) {
+value = Math.max(VALUE_MAX, value);
+}
+pressData[pos] = value;
 
 }
 
 export function createSavePassword(
-    wwaData: WWAData): string {
-    var map: Uint8Array;
-    if (pressData === void 0 || pressData === null) {
-        pressData = new Uint8Array(
-            wwaData.mapWidth * wwaData.mapWidth * 4
-        );
-    }
-
-    //        pressToBuffer();
-
-
-    return "";
+wwaData: WWAData): string {
+var map: Uint8Array;
+if (pressData === void 0 || pressData === null) {
+pressData = new Uint8Array(
+wwaData.mapWidth * wwaData.mapWidth * 4
+);
 }
+
+//        pressToBuffer();
+
+
+return "";
+}
+
