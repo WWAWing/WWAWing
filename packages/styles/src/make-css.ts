224049376b80d6b61c2b9082cf5164c16c2ec2f1
Aokashi <Aokashi.Towa@Gmail.com>
2018-11-06 00:01:44 +0900

    実行コマンド追加、coreをindexに変更

--- packages/styles/src/make-css.ts
+++ packages/styles/src/make-css.ts
@@ -1,9 +1,52 @@
 import * as sass from "node-sass";
+import * as path from "path";
+import * as fs from "fs";
 
-export function generateWWACSSFile() {
-    sass.render({
-        file: "core/core.scss"
-    }, function (err, result) {
-        // TODO: 作る
+const extendThemeNames = ["classic"];
+const SOURCE_DIRECTORY_NAME = "src";
+const OUTPUT_DIRECTORY_NAME = "output";
+const SOURCE_CORE_DIRECTORY_NAME = "core";
+const SOURCE_EXTENDS_DIRECOTRY_NAME = "extends";
+const INDEX_FILE_NAME = "index";
+
+const basePromise = createWWABaseCSSFilePromise();
+const extendPromises = createWWAExtendCSSFilePromises();
+
+Promise.all([basePromise].concat(extendPromises))
+    .catch(error => {
+        console.error("error", error);
+        process.exit(1);
     });
+
+function createWWABaseCSSFilePromise(): Promise<void> {
+    return createWWACSSFilePromise(
+        path.join(__dirname, "..", SOURCE_DIRECTORY_NAME, SOURCE_CORE_DIRECTORY_NAME, `${INDEX_FILE_NAME}.scss`),
+        path.join(__dirname, "..", OUTPUT_DIRECTORY_NAME, "wwa.css")
+    );
+}
+
+function createWWAExtendCSSFilePromises(): Promise<void>[] {
+    return extendThemeNames
+        .map(extendThemeName => createWWACSSFilePromise(
+            path.join(__dirname, "..", SOURCE_DIRECTORY_NAME, SOURCE_EXTENDS_DIRECOTRY_NAME, extendThemeName, `${INDEX_FILE_NAME}.scss`),
+            path.join(__dirname, "..", OUTPUT_DIRECTORY_NAME, `wwa_${extendThemeName}.css`)
+        ));
+}
+
+function createWWACSSFilePromise(filePath: string, outputFilePath: string): Promise<void> {
+    return new Promise((resolve, reject) => 
+        sass.render({
+            file: filePath
+        }, (err, result) => {
+            if (!err) {
+                fs.writeFile(
+                    outputFilePath,
+                    result.css,
+                    err => (err ? reject(err) : resolve())
+                );
+            } else {
+                reject(err);
+            }
+        })
+    );
 }
