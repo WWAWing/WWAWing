e0dd7515a93d18b94f2a25e88e56c09a8a9dcae5
Aokashi <Aokashi.Towa@Gmail.com>
2018-11-30 00:52:47 +0900

    auto-rotateの発生イベントをorientationchangeに変更、viewportの変更にheightを使用

--- packages/assets/scripts/auto_rotate.js
+++ packages/assets/scripts/auto_rotate.js
@@ -1,17 +1,17 @@
 function wwa_auto_rotate() {
     const WWA_WIDTH = 560;
-    const BUTTON_WIDTH = 100;
 
     let viewportElement = document.querySelector("meta[name='viewport']");
-    let viewportWidth = "";
+    let viewportValue = "";
 
+    // TODO: 下記の仕様だと、Firefox for Android では回転した際にレイアウトが崩れる。
     if (window.screen.width > window.screen.height) {
-        viewportWidth = WWA_WIDTH + (BUTTON_WIDTH * 4);
+        viewportValue = `height=${WWA_WIDTH}`;
     } else if (window.screen.width <= window.screen.height) {
-        viewportWidth = WWA_WIDTH;
+        viewportValue = `width=${WWA_WIDTH}`;
     }
-    viewportElement.setAttribute("content", `width=${viewportWidth},user-scalable=no`);
+    viewportElement.setAttribute("content", `${viewportValue},user-scalable=no`);
 }
 
 window.addEventListener("load", wwa_auto_rotate);
-window.addEventListener("resize", wwa_auto_rotate);
+window.addEventListener("orientationchange", wwa_auto_rotate);
