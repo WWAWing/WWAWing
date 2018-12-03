d4b19ac07aecbd1da1327134e7135e6bebf84298
Aokashi <Aokashi.Towa@Gmail.com>
2018-12-03 12:40:34 +0900

    auto-rotateのイベント発生元をorientationchangeからresizeに変更、縦横の取得元も変更

--- packages/assets/scripts/auto_rotate.js
+++ packages/assets/scripts/auto_rotate.js
@@ -1,17 +1,28 @@
 function wwa_auto_rotate() {
     const WWA_WIDTH = 560;
 
-    let viewportElement = document.querySelector("meta[name='viewport']");
+    const viewportElement = document.querySelector("meta[name='viewport']");
+    const browserWidth = window.innerWidth;
+    const browserHeight = window.innerHeight;
     let viewportValue = "";
 
-    // TODO: 下記の仕様だと、Firefox for Android では回転した際にレイアウトが崩れる。
-    if (window.screen.width > window.screen.height) {
-        viewportValue = `height=${WWA_WIDTH}`;
-    } else if (window.screen.width <= window.screen.height) {
+    if (browserWidth > browserHeight) {
+        const width = (browserWidth / browserHeight) * WWA_WIDTH;
+        viewportValue = `width=${width}`;
+    } else if (browserWidth <= browserHeight) {
         viewportValue = `width=${WWA_WIDTH}`;
     }
-    viewportElement.setAttribute("content", `${viewportValue},user-scalable=no`);
+    viewportElement.setAttribute("content", `${viewportValue}`);
 }
 
-window.addEventListener("load", wwa_auto_rotate);
-window.addEventListener("orientationchange", wwa_auto_rotate);
+function wwa_create_viewport() {
+    const headElement = document.getElementsByTagName('head')[0];
+    let viewportElement = document.createElement('meta');
+
+    viewportElement.setAttribute('name', 'viewport');
+    headElement.appendChild(viewportElement);
+    wwa_auto_rotate();
+}
+
+window.addEventListener("load", wwa_create_viewport);
+window.addEventListener("resize", wwa_auto_rotate);
