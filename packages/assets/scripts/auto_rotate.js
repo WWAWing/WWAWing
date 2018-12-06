bb0734e8b0d982c98f2e598283334f1c697f15e0
Aokashi <Aokashi.Towa@Gmail.com>
2018-12-06 16:49:23 +0900

    バーチャルパッドにノッチ部分の余白を追加(まだ動かないかも)

--- packages/assets/scripts/auto_rotate.js
+++ packages/assets/scripts/auto_rotate.js
@@ -1,5 +1,6 @@
 function wwa_auto_rotate() {
     const WWA_WIDTH = 560;
+    const WWA_HEIGHT = 440;
 
     const viewportElement = document.querySelector("meta[name='viewport']");
     const browserWidth = window.innerWidth;
@@ -7,12 +8,12 @@ function wwa_auto_rotate() {
     let viewportValue = "";
 
     if (browserWidth > browserHeight) {
-        const width = (browserWidth / browserHeight) * WWA_WIDTH;
+        const width = (browserWidth / browserHeight) * WWA_HEIGHT;
         viewportValue = `width=${width}`;
     } else if (browserWidth <= browserHeight) {
         viewportValue = `width=${WWA_WIDTH}`;
     }
-    viewportElement.setAttribute("content", `${viewportValue}`);
+    viewportElement.setAttribute("content", `${viewportValue}, viewport-fit=cover`);
 }
 
 function wwa_create_viewport() {
