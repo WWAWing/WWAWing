ced069edb332ff9570d21c479abf0e91e492b273
Shine <ukida-m@ukida.co.jp>
2019-08-30 17:34:20 +0900

    パスワードロードを使用しない場合に、データがありませんと表示される不具合を修正

--- packages/engine/src/wwa_main.ts
+++ packages/engine/src/wwa_main.ts
@@ -2904,6 +2904,7 @@ export class WWA {
                     switch (this._yesNoChoiceCallInfo) {
                         case ChoiceCallInfo.NONE:
                             (<HTMLDivElement>(util.$id(sidebarButtonCellElementID[SidebarButton.QUICK_LOAD]))).classList.remove("onpress");
+                            break;
                         case ChoiceCallInfo.CALL_BY_PASSWORD_LOAD:
                             this._yesNoJudge = YesNoState.UNSELECTED;
                             this.onpasswordloadcalled();
