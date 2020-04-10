20b78191195bf48edd1a1ab81124e09d28eb92b6
Matsuyuki <matsuyuki.eca@gmail.com>
2020-04-11 00:18:19 +0900

    fix(event-emitter): IE11で動かない不具合修正

--- packages/event-emitter/src/impl/browser/index.ts
+++ packages/event-emitter/src/impl/browser/index.ts
@@ -7,12 +7,12 @@ export class BrowserEventEmitter implements IEventEmitter {
   }
   dispatch<D>(eventName: string, data: D) {
    let customEvent: CustomEvent;
-    if (window["CustomEvent"]) {
+    if (typeof window["CustomEvent"] === "function") {
       customEvent = new CustomEvent(eventName, { detail: data });
     } else {
       // IE11 対応
       customEvent = document.createEvent('CustomEvent');
-      customEvent.initCustomEvent('eventName', false, false, data);
+      customEvent.initCustomEvent(eventName, false, false, data);
     }
     this.target.dispatchEvent(customEvent);
   }
