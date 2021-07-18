b408d2adc8c04234c8448b3176051f5e04be39a7
Matsuyuki <matsuyuki.eca@gmail.com>
2021-07-18 22:08:54 +0900

    feat(debug-server): 環境変数 WWA_SERVER_PORT でポートを選択できるようにした

--- packages/debug-server/src/index.ts
+++ packages/debug-server/src/index.ts
@@ -2,8 +2,17 @@ import express from "express";
 
 const baseDir = process.argv.length < 3 ? "." : process.argv[2];
 
+const DEFAULT_PORT = 3000;
+
+function parseServerPort(port: unknown): number {
+    if (Number.isNaN(port) || typeof port !== 'number') {
+      return DEFAULT_PORT;
+    }
+    return port;
+}
+
 const app = express();
-const SERVER_PORT = 3000;
+const SERVER_PORT = parseServerPort(process.env.WWA_SERVER_PORT);
 app.use("/", express.static(baseDir));
 app.listen(SERVER_PORT, () =>  {
     console.log("Welcome to WWA Server!");
