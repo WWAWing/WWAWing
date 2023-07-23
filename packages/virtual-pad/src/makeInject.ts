dc4f74feead054da4ca9feff8732dccb67d3b2d7
Aokashi <Aokashi.Towa@Gmail.com>
2023-07-23 18:31:24 +0900

    fix: 仮想パッドのボタン本体とボタン表面を別要素に分離

--- packages/virtual-pad/src/makeInject.ts
+++ packages/virtual-pad/src/makeInject.ts
@@ -1,16 +1,24 @@
 export default function makeInject() {
   return `
     <div id="wwa-virtualpad-left">
-      <button class="wwa-virtualpad__button" id="wwa-up-button"></button>
-      <button class="wwa-virtualpad__button" id="wwa-left-button"></button>
-      <button class="wwa-virtualpad__button" id="wwa-right-button"></button>
-      <button class="wwa-virtualpad__button" id="wwa-down-button"></button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button__surface" id="wwa-up-button"></button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button__surface" id="wwa-left-button"></button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button__surface" id="wwa-right-button"></button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button__surface" id="wwa-down-button"></button>
       <div class="wwa-virtualpad__hole"></div>
     </div>
     <div id="wwa-virtualpad-right">
-      <button class="wwa-virtualpad__button" id="wwa-slow-button">I</button>
-      <button class="wwa-virtualpad__button" id="wwa-fast-button">P</button>
-      <button class="wwa-virtualpad__button" id="wwa-enter-button">Y</button>
-      <button class="wwa-virtualpad__button" id="wwa-esc-button">N</button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button" id="wwa-slow-button">
+        <span class="wwa-virtualpad-button__surface">I</span>
+      </button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button__surface" id="wwa-fast-button">
+        <span class="wwa-virtualpad-button__surface">P</span>
+      </button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button--action" id="wwa-enter-button">
+        <span class="wwa-virtualpad-button__surface">Y</span>
+      </button>
+      <button class="wwa-virtualpad__button wwa-virtualpad-button wwa-virtualpad-button--action" id="wwa-esc-button">
+        <span class="wwa-virtualpad-button__surface">N</span>
+      </button>
     </div>`;
 }
