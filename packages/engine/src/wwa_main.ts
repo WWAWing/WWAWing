47896513df0f70969661af92613342caf9fbba85
Aokashi <aokashi.towa@gmail.com>
2025-02-27 21:52:24 +0900

    fix: カバー未指定だとWWAが起動しない問題の修正

--- packages/engine/src/wwa_main.ts
+++ packages/engine/src/wwa_main.ts
@@ -6849,7 +6849,7 @@ function start() {
             return mes;             // Gecko and WebKit
         }
     });
-    var titleImgName = util.$id("wwa-wrapper").getAttribute("data-wwa-title-img");
+    const titleImgName = util.$id("wwa-wrapper").getAttribute("data-wwa-title-img");
     const virtualPadAttribute = util.$id("wwa-wrapper").getAttribute("data-wwa-virtualpad-enable");
     const virtualPadEnable = virtualPadAttribute !== null && virtualPadAttribute.match(/^true$/i) !== null;
     inject(<HTMLDivElement>util.$id("wwa-wrapper"), titleImgName, virtualPadEnable);
@@ -6906,7 +6906,8 @@ function start() {
         {
             mapdata: mapFileName,
             urlGateEnable: urlgateEnabled,
-            titleImg: titleImgName,
+            // WWA のコンストラクターはカバー画像未指定の場合は undefined と扱う
+            titleImg: titleImgName ?? undefined,
             audioDir: audioDirectory,
             classicModeEnable: classicModeEnabled,
             itemEffectEnable: itemEffectEnabled,
